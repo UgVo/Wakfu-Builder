a0d010e475d128b267d1bf86111ae5f9b6067b8e
Ug.V. <32958860+UgVo@users.noreply.github.com>
2020-05-29 11:36:26 +0200

    Removed old and useless stuff

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,55 +10,10 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     datamanager.setDBManager(&database_manager);
     ui->actionCheck_new_version->setIcon(QIcon("images/divers/update32.png"));
-    item_model = new c_item_model(&database_manager,this);
-    model_proxy = new QSortFilterProxyModel;
-    model_proxy->setSourceModel(item_model);
-
-    item_table_layout = new QStackedLayout();
-    initModelProgress = new QProgressBar();
-    item_table_view = new QTableView();
-    item_table_bg = new QLabel();
-    movie = new QMovie("images/divers/loading.gif");
-    item_table_bg->setMovie(movie);
-    item_table_bg->setAlignment(Qt::AlignCenter);
-    movie->start();
-
-    item_table_view->setModel(model_proxy);
-    item_table_view->setSelectionBehavior(QAbstractItemView::SelectRows);
-    item_table_view->setSortingEnabled(true);
-    item_table_view->sortByColumn(0,Qt::AscendingOrder);
-
-    QVBoxLayout *tab_3_layout =  new QVBoxLayout;
-    item_table_layout->addWidget(item_table_view);
-    item_table_layout->addWidget(item_table_bg);
-    item_table_layout->setStackingMode(QStackedLayout::StackAll);
-
-    ui->tab_3->setLayout(tab_3_layout);
-    tab_3_layout->insertLayout(0,item_table_layout);
-    tab_3_layout->addWidget(initModelProgress);
 
     diag = new DialogGestion(&datamanager,datamanager.getVersion(),this);
 
     QObject::connect(ui->actionCheck_new_version,SIGNAL(triggered()),this,SLOT(slot_check_version_clicked()),Qt::UniqueConnection);
-    QObject::connect(item_model,SIGNAL(new_row(int,int)),this,SLOT(slot_new_row(int,int)));
-    QObject::connect(&m_watcher,&QFutureWatcher<void>::finished,this,&MainWindow::slot_init_done);
-
-    //m_watcher.setFuture(m_init = QtConcurrent::run(init,this));
-
-    builder = new c_builder_view(&database_manager);
-    builder->show();
-    test();
-}
-
-void MainWindow::init(MainWindow * MainWindow) {
-    QElapsedTimer timer;
-    timer.start();
-    timer.restart();
-    MainWindow->item_model->init();
-}
-
-void MainWindow::slot_init_done() {
-    item_table_layout->removeWidget(item_table_bg);
 }
 
 MainWindow::~MainWindow()
@@ -84,8 +39,6 @@ void MainWindow::test() {
     Q_ASSERT(!tok.formatString(QString("[ally] Applique [st5885] (1 par tour)"),{},200,4).compare("ally Applique [5885] (1 par tour)"));
     Q_ASSERT(tok.get_id_state("Applique [5885] (1 par tour)") == 5885);
     Q_ASSERT(tok.get_id_state("Applique (1 par tour)") == 0);
-
-    this->setWindowState(Qt::WindowMinimized);
 }
 
 void MainWindow::test_interpret_effect() {
@@ -102,38 +55,6 @@ void MainWindow::test_interpret_effect() {
      qDebug() << tok.interpret_effect("5 Niv. aux sorts Feu");
 }
 
-void MainWindow::slot_end_loading_model() {
-    QObject::disconnect(item_model,SIGNAL(new_row(int,int)),this,SLOT(slot_new_row(int,int)));
-    QObject::disconnect(initModel,SIGNAL(resultReady()),this,SLOT(slot_end_loading_model()));
-    item_table_layout->removeWidget(item_table_bg);
-    //item_table_view->setModel(model_proxy);
-    item_table_view->resizeColumnsToContents();
-    item_table_view->update();
-}
-
-void MainWindow::slot_new_row(int index,int size) {
-    movie->jumpToNextFrame();
-    initModelProgress->setMaximum(size);
-    initModelProgress->setValue(index+1);
-}
-
-void MainWindow::slot_update_item(QWidget* parent) {
-    parser = new ParseItemThread();
-    parser->setDatamanager(&datamanager);
-    //datamanager.moveToThread(&parser);
-    QObject::connect(&datamanager,SIGNAL(newItem(QString,int,int)),this,SLOT(slot_new_item(QString,int,int)));
-    progressDial = new QProgressDialog(parent);
-    progressDial->show();
-    parser->start();
-    qDebug() << "parse item end";
-}
-
-void MainWindow::slot_new_item(QString name, int index, int size) {
-    progressDial->setMaximum(size);
-    progressDial->setLabelText(tr("Insert %1. (%2/%3)").arg(name).arg(index+1).arg(size));
-    progressDial->setValue(index+1);
-}
-
 void MainWindow::slot_check_version_clicked() {
     datamanager.checkVersion();
     QObject::connect(&datamanager,SIGNAL(newVersion(QString)),this,SLOT(slot_version_check(QString)));
@@ -143,7 +64,6 @@ void MainWindow::slot_version_check(QString version) {
     QObject::disconnect(&datamanager,SIGNAL(newVersion(QString)),this,SLOT(slot_version_check(QString)));
     QString local_version = datamanager.getVersion();
     diag->set_version(version);
-    QObject::connect(diag,SIGNAL(updateItem(QWidget*)),this,SLOT(slot_update_item(QWidget*)));
     QString format = QString("<font color=\"%2\">%1</font>").arg(local_version,QColor::fromRgb(0,255,0).name());
     if (version != local_version) {
         diag->setText("New version available");
