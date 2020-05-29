0f254be2d8b06b0e6466fe8369aedc97e685774e
Mankio Nidar <mankio.nidar@gmail.com>
2020-05-29 11:46:56 +0200

    Delete useless objects of MainWindow

--- mainwindow.h
+++ mainwindow.h
@@ -59,34 +59,12 @@ private:
     Ui::MainWindow *ui;
     QString app_path;
     c_dbmanager database_manager;
-    c_networkManager network_manager;
     c_datamanager datamanager;
-    QProgressDialog *progressDial;
-    ParseItemThread *parser;
-    InitModelThread *initModel;
     DialogGestion *diag;
-    c_item_display *it_display;
-    c_item_model *item_model;
-    QSortFilterProxyModel *model_proxy;
-    QTableView *item_table_view;
-    QLabel *item_table_bg;
-    QStackedLayout *item_table_layout;
-    QProgressBar *initModelProgress;
-    QFuture<void> m_init;
-    QFutureWatcher<void> m_watcher;
-    QMovie *movie;
-   c_builder_view* builder;
 
 public slots:
-    void slot_update_item(QWidget *parent);
-    void slot_new_item(QString name, int index, int size);
     void slot_check_version_clicked();
     void slot_version_check(QString version);
-    void slot_new_row(int index,int size);
-    void slot_end_loading_model();
-    static void init(MainWindow *);
-    void slot_init_done();
-
 
     void test();
     void test_interpret_effect();
