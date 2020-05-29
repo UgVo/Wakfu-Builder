71d3a9ad43388ee137efc703e6fa2542c0820e22
Mankio Nidar <mankio.nidar@gmail.com>
2020-05-29 11:56:56 +0200

    Deleted useless debuging instruction

--- ui/c_search_widget.cpp
+++ ui/c_search_widget.cpp
@@ -261,14 +261,11 @@ void c_search_widget::slot_search() {
     QList<int> caract;
     if (ui->carac_search->currentIndex()!=0) caract.append(c_item::mapCaracToId[ui->carac_search->currentText()]);
     QList<QPair<int,int>> item_list_pair = dbmanager->getid_item_from_actions(caract,rarities,itemType, {ui->lvl_low->value(),ui->lvl_high->value()},ui->name_search->text());
-    qDebug() << item_list_pair.size();
     std::sort(item_list_pair.begin(),item_list_pair.end(),c_search_widget::compare_pair_id_lvl);
     QList<int> item_list;
     for(int i = 0; i < item_list_pair.size(); ++i) {
         item_list.push_back(item_list_pair.at(i).first);
     }
-    qDebug() << item_list.size();
-
     emit new_search_result(item_list);
 
 }
