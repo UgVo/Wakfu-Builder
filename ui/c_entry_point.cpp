88925ff63469905809b486c2475d9c54b6534720
Mankio <mankio.nidar@gmail.com>
2020-06-13 18:05:37 +0200

    Open animation completed

--- ui/c_entry_point.cpp
+++ ui/c_entry_point.cpp
@@ -42,6 +42,8 @@ void c_entry_point::resizeEvent(QResizeEvent * /*event*/) {
 }
 
 void c_entry_point::slot_open_button() {
+    ui->pushButton_new_build->setEnabled(false);
+    ui->pushButton_open_build->setEnabled(false);
     ui->pushButton_open_build->raise();
     QPoint translation = QPoint(((ui->pushButton_new_build->width()*2)+6)/2 - ui->pushButton_new_build->width()/2,0);
     animation1 = new QPropertyAnimation(ui->pushButton_new_build,"geometry");
@@ -75,6 +77,12 @@ void c_entry_point::slot_open_button() {
 }
 
 void c_entry_point::slot_new_button() {
+    QRect rect = this->rect();
+    QPoint p_pb_new_build((rect.width()-506)/2,(rect.height()-172)/2);
+    QPoint p_pb_new_build_final((rect.width()-250)/2,(rect.height()-172)/2);
+    QPoint p_pb_open_build((rect.width()-506)/2 + 256,(rect.height()-172)/2);
+    QPoint p_pb_open_build_final = p_pb_new_build_final;
+
     ui->label_creation_builder->setText("Création du builder ...");
 
     effect1 = new QGraphicsOpacityEffect(ui->background_eliacube);
@@ -84,7 +92,6 @@ void c_entry_point::slot_new_button() {
     animation1->setStartValue(1.0);
     animation1->setEndValue(0.0);
     animation1->setEasingCurve(QEasingCurve::OutQuad);
-    qDebug() << "anim1";
 
     effect2 = new QGraphicsOpacityEffect(ui->pushButton_open_build);
     ui->pushButton_open_build->setGraphicsEffect(effect2);
@@ -93,33 +100,27 @@ void c_entry_point::slot_new_button() {
     animation2->setStartValue(1.0);
     animation2->setEndValue(0.0);
     animation2->setEasingCurve(QEasingCurve::OutQuad);
-    qDebug() << "anim2";
 
     ui->pushButton_new_build->raise();
-    QPoint translation = QPoint(((ui->pushButton_new_build->width()*2)+6)/2 - ui->pushButton_new_build->width()/2,0);
     animation3 = new QPropertyAnimation(ui->pushButton_new_build,"geometry");
     animation3->setDuration(anim_duration_ms);
-    QRect rect_button_new_p0 = ui->pushButton_new_build->rect();
-    QPoint position_button_new_0 = ui->pushButton_new_build->pos();
-    rect_button_new_p0.setTopLeft(position_button_new_0);
-    animation3->setKeyValueAt(0,rect_button_new_p0);
-    animation3->setKeyValueAt(0.5,rect_button_new_p0);
-    rect_button_new_p0.setTopLeft(position_button_new_0 + translation);
-    animation3->setKeyValueAt(1,rect_button_new_p0);
+    QRect rect_button_new = ui->pushButton_new_build->rect();
+    rect_button_new.setTopLeft(p_pb_new_build);
+    animation3->setKeyValueAt(0,rect_button_new);
+    animation3->setKeyValueAt(0.5,rect_button_new);
+    rect_button_new.setTopLeft(p_pb_new_build_final);
+    animation3->setKeyValueAt(1,rect_button_new);
     animation3->setEasingCurve(QEasingCurve::InOutQuad);
-    qDebug() << "anim3";
 
     animation4 = new QPropertyAnimation(ui->pushButton_open_build,"geometry");
     animation4->setDuration(anim_duration_ms);
-    QRect rect_button_p0 = ui->pushButton_open_build->rect();
-    QPoint position_button_0 = ui->pushButton_open_build->pos();
-    rect_button_p0.setTopLeft(position_button_0);
-    animation4->setKeyValueAt(0,rect_button_p0);
-    animation4->setKeyValueAt(0.5,rect_button_p0);
-    rect_button_p0.setTopLeft(position_button_0 - translation);
-    animation4->setKeyValueAt(1,rect_button_p0);
+    QRect rect_button_open = ui->pushButton_open_build->rect();
+    rect_button_open.setTopLeft(p_pb_open_build);
+    animation4->setKeyValueAt(0,rect_button_open);
+    animation4->setKeyValueAt(0.5,rect_button_open);
+    rect_button_open.setTopLeft(p_pb_open_build_final);
+    animation4->setKeyValueAt(1,rect_button_open);
     animation4->setEasingCurve(QEasingCurve::InOutQuad);
-    qDebug() << "anim4";
 
     parralle_anim = new QParallelAnimationGroup;
     parralle_anim->addAnimation(animation1);
@@ -138,6 +139,11 @@ void c_entry_point::slot_first_anim_finished() {
 }
 
 void c_entry_point::slot_creation_builder_anim() {
+    ui->label_creation_builder->hide();
+    QRect rect = this->rect();
+    QPoint p_pb_new_build((rect.width()-250)/2,(rect.height()-172)/2);
+    QPoint p_pb_new_build_final((rect.width()-250)/2,rect.height());
+
     effect2 = new QGraphicsOpacityEffect(this);
     this->setGraphicsEffect(effect2);
     animation2 = new QPropertyAnimation(effect2, "opacity");
@@ -145,23 +151,19 @@ void c_entry_point::slot_creation_builder_anim() {
     animation2->setStartValue(1.0);
     animation2->setEndValue(0.0);
     animation2->setEasingCurve(QEasingCurve::OutQuad);
-    qDebug() << "anim5";
 
-    QPoint translation = QPoint(0,this->height());
-    animation3 = new QPropertyAnimation(this,"geometry");
-    animation3->setDuration(anim_duration_ms);
-    QRect rect_button_new_p0 = this->rect();
-    QPoint position_button_new_0 = this->pos();
-    rect_button_new_p0.setTopLeft(position_button_new_0);
-    animation3->setKeyValueAt(0,rect_button_new_p0);
-    rect_button_new_p0.setTopLeft(position_button_new_0 + translation);
-    animation3->setKeyValueAt(1,rect_button_new_p0);
-    animation3->setEasingCurve(QEasingCurve::InOutQuad);
-    qDebug() << "anim6";
+    animation4 = new QPropertyAnimation(ui->pushButton_new_build,"geometry");
+    animation4->setDuration(anim_duration_ms);
+    QRect rect_button_new = ui->pushButton_new_build->rect();
+    rect_button_new.setTopLeft(p_pb_new_build);
+    animation4->setKeyValueAt(0,rect_button_new);
+    rect_button_new.setTopLeft(p_pb_new_build_final);
+    animation4->setKeyValueAt(1,rect_button_new);
+    animation4->setEasingCurve(QEasingCurve::InOutQuad);
 
     parralle_anim = new QParallelAnimationGroup;
     parralle_anim->addAnimation(animation2);
-    parralle_anim->addAnimation(animation3);
+    parralle_anim->addAnimation(animation4);
 
     parralle_anim->start(QParallelAnimationGroup::DeleteWhenStopped);
 
@@ -169,6 +171,5 @@ void c_entry_point::slot_creation_builder_anim() {
 }
 
 void c_entry_point::slot_second_anim_finished() {
-    qDebug() << "anim7";
     emit second_animation_finished();
 }
