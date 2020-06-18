f8d973d2f0305e78362a82e6f4247c480f3ef043
Mankio <mankio.nidar@gmail.com>
2020-06-18 15:28:43 +0200

    last part of open animation at entrey_point

--- ui/c_entry_point.cpp
+++ ui/c_entry_point.cpp
@@ -70,7 +70,7 @@ void c_entry_point::resizeEvent(QResizeEvent * /*event*/) {
         p_w_bdd = QPoint((rect.width()-1210)/2 + 610,(rect.height()-150-400+20)/2 + 170);
         p_pb_new_build = QPoint(20,20);
         p_pb_open_build = QPoint((rect.width()-250)/2,(rect.height()-150-400+20)/2);
-        p_label_creation_builder = QPoint((rect.width()-106)/2,(rect.height()-172)/2 + 156);
+        p_label_creation_builder = QPoint((rect.width()-120)/2,(rect.height()-172)/2 + 156);
     }
     ui->pushButton_new_build->move(p_pb_new_build);
     ui->pushButton_open_build->move(p_pb_open_build);
@@ -173,6 +173,9 @@ void c_entry_point::slot_open_button() {
         parralle_anim->start(QParallelAnimationGroup::DeleteWhenStopped);
 
         state = 2;
+
+        QObject::connect(file_dial,&QFileDialog::rejected,this,&c_entry_point::slot_cancel_open);
+        QObject::connect(load_builder,&c_load_builder_dialog::rejected,this,&c_entry_point::slot_cancel_open);
     }
 }
 
@@ -340,9 +343,19 @@ void c_entry_point::slot_creation_builder_anim() {
     animation4->setKeyValueAt(1,rect_button_new);
     animation4->setEasingCurve(QEasingCurve::InOutQuad);
 
+    animation1 = new QPropertyAnimation(ui->pushButton_open_build,"geometry");
+    animation1->setDuration(anim_duration_ms);
+    QRect rect_button_open = ui->pushButton_open_build->rect();
+    rect_button_new.setTopLeft(p_pb_new_build);
+    animation1->setKeyValueAt(0,rect_button_new);
+    rect_button_new.setTopLeft(p_pb_new_build_final);
+    animation1->setKeyValueAt(1,rect_button_new);
+    animation1->setEasingCurve(QEasingCurve::InOutQuad);
+
     parralle_anim = new QParallelAnimationGroup;
     parralle_anim->addAnimation(animation2);
     parralle_anim->addAnimation(animation4);
+    parralle_anim->addAnimation(animation1);
 
     parralle_anim->start(QParallelAnimationGroup::DeleteWhenStopped);
 
@@ -354,6 +367,23 @@ void c_entry_point::slot_second_anim_finished() {
 }
 
 void c_entry_point::slot_open_from_source() {
+    qDebug() << "accepted";
+    qDebug() << sender()->metaObject()->className();
+    QObject* sender_widget = sender();
+    QRect rect = this->rect();
+    QPoint p_w_file_final((rect.width()-1210)/2,rect.height());
+    QPoint p_w_file((rect.width()-1210)/2,(rect.height()-150-400+20)/2 + 170);
+    QPoint p_w_bdd_final((rect.width()-1210)/2 + 610,rect.height());
+    QPoint p_w_bdd((rect.width()-1210)/2 + 610,(rect.height()-150-400+20)/2 + 170);
+    QPoint p_pb_open_build_final((rect.width()-250)/2,(rect.height()-172)/2);
+    QPoint p_pb_open_build((rect.width()-250)/2,(rect.height()-150-400+20)/2);
+    if (dynamic_cast<QFileDialog*>(sender_widget) != nullptr) {
+        format = c_io_manager::jsonformat::file;
+        path_json = file_dial->selectedFiles().at(0);
+    } else {
+        format = c_io_manager::jsonformat::database;
+        path_json = load_builder->getCurrent_json();
+    }
     load_builder->show();
     file_dial->show();
 
@@ -365,9 +395,134 @@ void c_entry_point::slot_open_from_source() {
     animation1->setEndValue(0.0);
     animation1->setEasingCurve(QEasingCurve::OutQuad);
 
+    animation2 = new QPropertyAnimation(ui->frame_bdd,"geometry");
+    animation2->setDuration(anim_duration_ms);
+    QRect rect_bdd = ui->frame_bdd->rect();
+    rect_bdd.setTopLeft(p_w_bdd);
+    animation2->setKeyValueAt(0,rect_bdd);
+    animation2->setKeyValueAt(0.5,rect_bdd);
+    rect_bdd.setTopLeft(p_w_bdd_final);
+    animation2->setKeyValueAt(1,rect_bdd);
+    animation2->setEasingCurve(QEasingCurve::InOutQuad);
+
+    animation3 = new QPropertyAnimation(ui->frame_file,"geometry");
+    animation3->setDuration(anim_duration_ms);
+    QRect rect_file = ui->frame_file->rect();
+    rect_file.setTopLeft(p_w_file);
+    animation3->setKeyValueAt(0,rect_file);
+    animation3->setKeyValueAt(0.5,rect_file);
+    rect_file.setTopLeft(p_w_file_final);
+    animation3->setKeyValueAt(1,rect_file);
+    animation3->setEasingCurve(QEasingCurve::InOutQuad);
+
+    animation4 = new QPropertyAnimation(ui->pushButton_open_build,"geometry");
+    animation4->setDuration(anim_duration_ms);
+    QRect rect_button_open = ui->pushButton_open_build->rect();
+    rect_button_open.setTopLeft(p_pb_open_build);
+    animation4->setKeyValueAt(0,rect_button_open);
+    animation4->setKeyValueAt(0.5,rect_button_open);
+    rect_button_open.setTopLeft(p_pb_open_build_final);
+    animation4->setKeyValueAt(1,rect_button_open);
+    animation4->setEasingCurve(QEasingCurve::InOutQuad);
+
+    effect1 = new QGraphicsOpacityEffect(ui->background_eliacube);
+    ui->background_eliacube->setGraphicsEffect(effect1);
+    animation5 = new QPropertyAnimation(effect1, "opacity");
+    animation5->setDuration(anim_duration_ms);
+    animation5->setStartValue(1.0);
+    animation5->setEndValue(0.0);
+    animation5->setEasingCurve(QEasingCurve::OutQuad);
+
     parralle_anim = new QParallelAnimationGroup;
     parralle_anim->addAnimation(animation1);
-    //parralle_anim->addAnimation(animation4);
+    parralle_anim->addAnimation(animation2);
+    parralle_anim->addAnimation(animation3);
+    parralle_anim->addAnimation(animation4);
+    parralle_anim->addAnimation(animation5);
 
     parralle_anim->start(QParallelAnimationGroup::DeleteWhenStopped);
+
+    ui->label_creation_builder->setText("Ouverture du builder...");
+    ui->label_creation_builder->hide();
+    QObject::connect(parralle_anim,&QParallelAnimationGroup::finished,ui->pushButton_new_build,&QPushButton::hide);
+    QObject::connect(parralle_anim,&QParallelAnimationGroup::finished,ui->label_creation_builder,&QLabel::show);
+    QObject::connect(parralle_anim,&QParallelAnimationGroup::finished,this,&c_entry_point::slot_open_animation_finished);
+}
+
+void c_entry_point::slot_cancel_open() {
+    load_builder->show();
+    file_dial->show();
+
+    QRect rect = this->rect();
+    QPoint p_w_file_final((rect.width()-1210)/2,rect.height());
+    QPoint p_w_file((rect.width()-1210)/2,(rect.height()-150-400+20)/2 + 170);
+    QPoint p_w_bdd_final((rect.width()-1210)/2 + 610,rect.height());
+    QPoint p_w_bdd((rect.width()-1210)/2 + 610,(rect.height()-150-400+20)/2 + 170);
+    QPoint p_pb_new_build_final((rect.width()-506)/2,(rect.height()-172)/2);
+    QPoint p_pb_new_build(20,20);
+    QPoint p_pb_open_build_final((rect.width()-506)/2 + 256,(rect.height()-172)/2);
+    QPoint p_pb_open_build((rect.width()-250)/2,(rect.height()-150-400+20)/2);
+
+    animation1 = new QPropertyAnimation(ui->pushButton_new_build,"geometry");
+    animation1->setDuration(anim_duration_ms);
+    QRect rect_button_new = ui->pushButton_new_build->rect();
+    rect_button_new.setTopLeft(p_pb_new_build);
+    animation1->setKeyValueAt(0,rect_button_new);
+    animation1->setKeyValueAt(0.5,rect_button_new);
+    rect_button_new.setTopLeft(p_pb_new_build_final);
+    animation1->setKeyValueAt(1,rect_button_new);
+    animation1->setEasingCurve(QEasingCurve::InOutQuad);
+
+    animation2 = new QPropertyAnimation(ui->pushButton_new_build,"size");
+    animation2->setDuration(anim_duration_ms);
+    QSize size_button_new_size = ui->pushButton_new_build->size()*2;
+    animation2->setKeyValueAt(0,ui->pushButton_new_build->size());
+    animation2->setKeyValueAt(0.5,ui->pushButton_new_build->size());
+    animation2->setKeyValueAt(1,size_button_new_size);
+    animation2->setEasingCurve(QEasingCurve::InOutQuad);
+
+    animation3 = new QPropertyAnimation(ui->pushButton_open_build,"geometry");
+    animation3->setDuration(anim_duration_ms);
+    QRect rect_button_open = ui->pushButton_open_build->rect();
+    rect_button_open.setTopLeft(p_pb_open_build);
+    animation3->setKeyValueAt(0,rect_button_open);
+    animation3->setKeyValueAt(0.5,rect_button_open);
+    rect_button_open.setTopLeft(p_pb_open_build_final);
+    animation3->setKeyValueAt(1,rect_button_open);
+    animation3->setEasingCurve(QEasingCurve::InOutQuad);
+
+    animation4 = new QPropertyAnimation(ui->frame_bdd,"geometry");
+    animation4->setDuration(anim_duration_ms);
+    QRect rect_bdd = ui->frame_bdd->rect();
+    rect_bdd.setTopLeft(p_w_bdd);
+    animation4->setKeyValueAt(0,rect_bdd);
+    animation4->setKeyValueAt(0.5,rect_bdd);
+    rect_bdd.setTopLeft(p_w_bdd_final);
+    animation4->setKeyValueAt(1,rect_bdd);
+    animation4->setEasingCurve(QEasingCurve::InOutQuad);
+
+    animation5 = new QPropertyAnimation(ui->frame_file,"geometry");
+    animation5->setDuration(anim_duration_ms);
+    QRect rect_file = ui->frame_file->rect();
+    rect_file.setTopLeft(p_w_file);
+    animation5->setKeyValueAt(0,rect_file);
+    animation5->setKeyValueAt(0.5,rect_file);
+    rect_file.setTopLeft(p_w_file_final);
+    animation5->setKeyValueAt(1,rect_file);
+    animation5->setEasingCurve(QEasingCurve::InOutQuad);
+
+    parralle_anim = new QParallelAnimationGroup;
+    parralle_anim->addAnimation(animation1);
+    parralle_anim->addAnimation(animation2);
+    parralle_anim->addAnimation(animation3);
+    parralle_anim->addAnimation(animation4);
+    parralle_anim->addAnimation(animation5);
+
+    parralle_anim->start(QParallelAnimationGroup::DeleteWhenStopped);
+
+    state = 0;
+}
+
+void c_entry_point::slot_open_animation_finished() {
+    emit load_builder_from(format,path_json);
 }
