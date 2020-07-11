e4885bb17b06397a86ab026933c69c016e1f4770
Ug.V. <32958860+UgVo@users.noreply.github.com>
2020-07-11 20:04:49 +0200

    calculus class enhanced to compute raw power at each build change

--- src/utilities/c_calcul.h
+++ src/utilities/c_calcul.h
@@ -1,15 +1,31 @@
 #ifndef C_CALCUL_H
 #define C_CALCUL_H
 
+#include <QObject>
 #include <QtMath>
 #include <QDebug>
 
-class c_calcul
+class c_build;
+
+class c_calcul : public QObject
 {
+    Q_OBJECT
+
 public:
     c_calcul();
     static int compute_reduction(const int resistances);
     static int compute_life(const int niveau);
+    void setBuild(c_build* build);
+
+public slots:
+    void computeMainMastery();
+    void computeDamage(int attaqueValue);
+
+private:
+    c_build *_build;
+    QList<QString> _mainMastery;
+    QMap<QString,qreal> _damages;
+    QString _healingElementMastery;
 };
 
 #endif // C_CALCUL_H
